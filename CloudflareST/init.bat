f072a6a81abe9a9bd909c0957b52fd87cf56cd1f
c332030 <c332030@gmail.com>
2023-01-31 14:30:56 +0800

    feat: init

--- CloudflareST/init.bat
+++ CloudflareST/init.bat
@@ -1,18 +1,20 @@
 @echo off
 
 call link-file "%~dp0data" "%windir%\System32\drivers\etc" hosts
+
 call link-file "%~dp0data" "%~dp0app" nowip_hosts.txt
+call link-file "%~dp0data" "%~dp0app" result_hosts.txt
 
 set SCHTASKS_NAME=CloudflareST
 
 ::schtasks /Delete /F /TN %SCHTASKS_NAME%
 
-echo.
-schtasks /Create /F ^
-  /TN %SCHTASKS_NAME% ^
-  /TR %~dp0update-ip.bat ^
-  /ST 00:00 ^
-  /SC HOURLY
+:: echo.
+:: schtasks /Create /F ^
+::   /TN %SCHTASKS_NAME% ^
+::   /TR %~dp0update-ip.bat ^
+::   /ST 00:00 ^
+::   /SC HOURLY
 ::  /RI 60
 
 pause >nul
