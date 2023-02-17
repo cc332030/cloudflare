1bb3a5b79de8718ce08e549bffa33bc57ece503a
c332030 <c332030@gmail.com>
2023-02-17 17:02:14 +0800

    feat: email-worker

---
+++ workers/email-worker/main.js
@@ -0,0 +1,21 @@
+
+
+// wrangler tail c332030-email-worker
+export default {
+  async email(message, env, ctx) {
+
+    const addresses = [
+      'c332030@qq.com',
+      'c332030@gmail.com',
+      'c332030@msn.cn'
+    ]
+
+    const toAddress = message.headers.get("to")
+
+    for(const address of addresses) {
+      if(address !== toAddress) {
+        await message.forward(address)
+      }
+    }
+  }
+}
